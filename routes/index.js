06dab14240a6bd12fab2563c3c91523f8b7c2faf
Jose Mathew <jos.india@yahoo.com>
2015-10-14 22:22:02 -0400

    lastCommit

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'First Express Generator Site',
-                        message: 'Our first page'} );
+  res.render('index', {} );
+});
+
+router.get('/index', function (req, res, next) {
+    res.render('index', {
+        
+    });
 });
 
 
-/* GET ABOUT page */
-router.get('/about', function(req, res, next) {
-   res.render('about'); 
-});
 
 router.get('/Contact', function(req, res, next) {
    res.render('Contact'); 
 });
 
-router.get('/services', function(req, res, next) {
-   res.render('services'); 
-});
-
-router.get('/Projects', function(req, res, next) {
-   res.render('Projects'); 
-});
 
 
 module.exports = router;
